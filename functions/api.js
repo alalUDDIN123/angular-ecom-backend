03c6772a11bdf17010d59f3a8238a94fab10b865
Alal Uddin <alalu75666@gmail.com>
2026-01-28 22:39:55 +0530

    express version updated

--- functions/api.js
+++ functions/api.js
@@ -1,22 +1,4 @@
 const serverless = require("serverless-http");
-const dotenv = require("dotenv");
-const connectDB = require("../src/config/db");
 const app = require("../src/app");
 
-dotenv.config();
-
-let isConnected = false;
-
-/**
- * Netlify function handler
- */
-const handler = async (event, context) => {
-  if (!isConnected) {
-    await connectDB();
-    isConnected = true;
-  }
-
-  return serverless(app)(event, context);
-};
-
-module.exports = { handler };
+module.exports.handler = serverless(app);
